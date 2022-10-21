0237279c3a8a2832caf6a03b06d1c38e3b6d96c1
Alexander <sanya.kashin.2000@mail.ru>
2022-10-21 22:00:03 +0300

    готова 3 задача

--- zadacha_3/Program.cs
+++ zadacha_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите число");
+string numberstr = Console.ReadLine() ??"";
+int num = int.Parse(numberstr);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"число {num} делится без остатка");
+}
+else
+{
+    Console.WriteLine($"число {num} не делится без остатка");
+}
